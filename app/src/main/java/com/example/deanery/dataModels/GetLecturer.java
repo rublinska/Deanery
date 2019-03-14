7cab033d26d1485ac57a0b5f12a60865aa647f0d
Єлизавета Богачек <bohachek@ukma.edu.ua>
2019-03-14 17:02:10 +0200

    web services in progress

---
+++ app/src/main/java/com/example/deanery/dataModels/GetLecturer.java
@@ -0,0 +1,39 @@
+
+package com.example.deanery.dataModels;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class GetLecturer {
+
+    @SerializedName("data")
+    @Expose
+    private Lecturer lecturer;
+    @SerializedName("status")
+    @Expose
+    private Boolean status;
+
+    public GetLecturer(Lecturer lecturer, Boolean status) {
+        this.lecturer = lecturer;
+        this.status = status;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+}
